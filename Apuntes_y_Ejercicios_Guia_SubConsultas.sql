b4a03a34cccf36d3f53e19240590762d9181c779
Manuel Rey Laguarde <manuelreylaguarde@gmail.com>
2025-11-20 10:51:58 +0100

    avanzo con la guia de subconsultas

--- Apuntes_y_Ejercicios_Guia_SubConsultas.sql
+++ Apuntes_y_Ejercicios_Guia_SubConsultas.sql
@@ -35,6 +35,7 @@ WHERE
 -- ------------------------------------------------------------------------------------------
 -- =====USO DE 'IN' PARA SUBCONSULTAS QUE DEVUELVEN CONJUNTOS=====
 -- usamos IN si la subconsulta puede devolver múltiples filas (o su negación NOT IN)
+-- lo usamos cuando no nos vale un operador porque tenemos que comparar dos cosas
 
 -- obtener los nombres de todos los actores que actuan en la pelicula "ALONE TRIP"
 
@@ -129,6 +130,8 @@ WHERE
 
 -- si hay coincidencia pasa el filtro de exist y devuelve el cliente que tiene al menos un alquiler
 
+-- ------------------------------------------------------------------------------------------
+
 -- ===NOT EXISTS===
 -- peliculas (con sus copias) que nunca han sido alquiladas
 
@@ -147,6 +150,8 @@ WHERE
             i.film_id = f.film_id); -- vuelve a comparar la existencia  del alquiler con la pelicula mediante film_id pero ahora devuelve el dato si este no existe
 
 -- el resultado es la lista de titulos de las peliculas nunca rentadas
+
+-- ------------------------------------------------------------------------------------------
 
 -- Otro uso típico de subconsultas correlacionadas con NOT EXISTS es para preguntas del tipo "para cada X
 -- que cumpla una condicion con todos los Y relacionados"
@@ -177,6 +182,8 @@ WHERE
 
 -- lo importante es notar la doble negacion "no existe una categoria tal que no exista una pelicula de ese actor en dicha categoria"
 -- equivale a "el actor tiene al menos una película en cada categoría"
+
+-- ------------------------------------------------------------------------------------------
 
 -- ====2.1 Algunos ejemplos mas====
 
@@ -218,7 +225,9 @@ WHERE
                     category
                 WHERE
                     name = 'Children')); -- osea todas las peliculas cuyo id esté relacionado con el id '3' de la categoria (que pertenece a children)
-                    
+        
+-- ------------------------------------------------------------------------------------------       
+        
 -- SUBCONSULTAS CORRELACIONADAS CON EXISTS
 
 -- a veces la condicion de filtrado requiere verificar la existencia o ausencia de las filas relacionadas
@@ -268,6 +277,8 @@ WHERE
         WHERE
             i.film_id = f.film_id)
 ORDER BY f.title;
+
+-- ------------------------------------------------------------------------------------------
 
 -- Ejemplo 3: Actores con alguna película de length > 120 (camino y pasos)
 -- camino de tablas: actor->film_actor->film
@@ -319,6 +330,8 @@ WHERE
                 AND f.length > 120)
 ORDER BY a.last_name , a.first_name;
 
+-- ------------------------------------------------------------------------------------------
+
 -- ====SUBCONSULTAS EN LA LISTA SELECT (campos calculados por fila)====
 
 -- se colocan en el select como parte de columnas calculadas en la salida, deben ser de tipo escalar (devuelven un unico valor por cada fila externa)
@@ -339,4 +352,297 @@ FROM
 
 -- esta manera es equivalente a hacer un join con rental e inventory y agruparlo con film
 -- pero a veces la subconsulta resulta más sencilla de entender "para esta pelicula, calcula este dato agregado"
+
+-- ------------------------------------------------------------------------------------------
+
+-- Ej: listar cada cliente con el monto total que ha pagado en todos sus alquileres
+
+SELECT 
+    c.first_name,
+    c.last_name,
+    (SELECT 
+            SUM(p.amount)
+        FROM
+            payment p
+               -- JOIN
+           -- rental r USING (rental_id)
+        WHERE
+            p.customer_id = c.customer_id) AS total_pagado
+FROM
+    customer c;
+    
+-- las subconsultas correlacionadas pueden emplearse en la cláusula WHERE (usando EXISTS, NOT EXISTS, IN,etc)
+-- o el la lista select como subconsultas escalares para crear columnas calculadas
+
+-- ------------------------------------------------------------------------------------------
+
+-- =========3 Subconsultas en la cláusula FROM (tablas derivadas)=========
+
+-- es útil cuando queremos estructurar la consulta en pasos: primero obtener un resultado intermedio con su propia SELECT
+-- y luego onsultarlo o combinarlo con otras tablas.
+-- Es obligatorio asignarle un alias (porque la consulta externa debe referirse a ese resultado)
+
+-- Ejemplo: queremos encontrar las categorías de películas que tienen más películas que el número promedio de peliculas por categoria.
+-- (categorías que estan "por encima del promedio" en cuanto a cantidad de películas)
+
+-- con agregaciones requeriria dos niveles de agrupamiento, primero contar películas por categoría, luego comparar cada conteo
+-- con el promedio de todos los conteos. SQL no permite esto a menos que usemos una subconsulta
+
+-- Pasos:
+-- 1.- Obtener el número de películas por categoría
+-- 2.- Calcular el promedio de esos números
+-- 3.- Seleccionar las categorias cuyo número está por encima de ese promedio
+
+-- 1.- Construimos una subconsulta que calcule el total de películas en cada categoría.
+
+SELECT 
+    category_id, COUNT(*) AS film_count
+FROM
+    film_category
+GROUP BY category_id;
+
+-- esta subconsulta produce una tabla con dos columnas: el id y el total de películas de ese id (de esa categoría)
+
+-- ahora la usaremos dentro de la clausula FROM de la consulta principal, aliñandola e incorporamos el cálculo del promedio
+
+SELECT 
+    ca.name AS category_name, category_counts.film_count
+FROM
+    (SELECT 
+        category_id, COUNT(*) AS film_count
+    FROM
+        film_category
+    GROUP BY category_id) AS category_counts -- es la subconsulta derivada qque saca el total de películas por categoría. AHORA SE COMPORTA COMO UNA TABLA llamada category_counts
+        JOIN
+    category ca ON ca.category_id = category_counts.category_id -- hacemos un join de la nueva tabla, con category, para obtener el nombre de la categoria
+WHERE -- aca comparamos el resultado de contar todas las peliculas por categoria (osea todas las películas que pertenecen a esa categoria) >>>
+    category_counts.film_count > (SELECT 
+            AVG(film_count) -- >>> con el promedio  que se obtiene mediante otra subconsulta que es igual que la anterior pero ahora para calcular el promedio
+        FROM
+            (SELECT 
+                category_id, COUNT(*) AS film_count
+            FROM
+                film_category
+            GROUP BY category_id) AS category_counts2);
+-- la consulta final lista el nombre de cada categoría y su conteo de películas, para las categorías sobre la media
+
+-- Nota: Esta solucion nos obliga a repetir la subconsulta de conteo dos veces.
+-- En SQL existen formas alternativas. podriamos haber calculado el total de peliculas (N), y el número de categorias (K) para computar la media en una sola subconsutla
+-- o utilizar una WITH
+
+-- ------------------------------------------------------------------------------------------
+
+-- Otro ejemplo de subconsulta con FROM es usarla para lograr un "paso intermedio"
+-- Podríamos reescribir la consulta de "películas con duracion superior a la media" de la siguiente manera:
+
+SELECT 
+    f.title, f.length
+FROM
+    film f
+        JOIN
+    (SELECT 
+        AVG(length) AS avg_length
+    FROM
+        film) AS stats
+WHERE
+    f.length > stats.avg_length;
+    
+-- En general las subconsultas en FROM son muy utiles para:
+-- 		descomponer consultas complejas en subpasos comprensibles
+--  	evitarl limitaciones del SQL básico
+-- 		Reutilizar resultados intermedios
+
+-- ------------------------------------------------------------------------------------------
+-- =====CONCLUSIONES=====
+-- ------------------------------------------------------------------------------------------
+-- =======EJERCICIOS DE PRACTICA=======
+
+-- Ejercicio 1: Películas por encima de la duración promedio
+
+SELECT 
+    f.title, f.length
+FROM
+    film f
+WHERE
+    length > (SELECT 
+            AVG(f.length)
+        FROM
+            film f);
+-- ORDER BY f.length DESC , f.title;
+
+-- Ejercicio 2: Actores en "Action" y en "Comedy"
+
+SELECT 
+    a.first_name, a.last_name
+FROM
+    actor a
+WHERE
+    EXISTS( SELECT 
+            1
+        FROM
+            film_actor fa
+                JOIN
+            film_category fc USING (film_id)
+                JOIN
+            category c USING (category_id)
+        WHERE
+            fa.actor_id = a.actor_id
+                AND c.name = 'Action')
+        AND EXISTS( SELECT 
+            1
+        FROM
+            film_actor fa
+                JOIN
+            film_category fc USING (film_id)
+                JOIN
+            category c USING (category_id)
+        WHERE
+            fa.actor_id = a.actor_id
+                AND c.name = 'Comedy')
+ORDER BY a.last_name , a.first_name;
+
+-- Ejercicio 3: Cliente con más alquileres
+
+SELECT 
+    c.customer_id, c.first_name, c.last_name
+FROM
+    customer AS c
+        JOIN
+    (SELECT 
+        r.customer_id, COUNT(*) AS rentals
+    FROM
+        rental AS r
+    GROUP BY r.customer_id) AS t ON t.customer_id = c.customer_id
+WHERE
+    t.rentals = (SELECT 
+            MAX(x.rentals)
+        FROM
+            (SELECT 
+                r2.customer_id, COUNT(*) AS rentals
+            FROM
+                rental AS r2
+            GROUP BY r2.customer_id) AS x);
+
+-- Ejercicio 4: Películas de "Sports" no alquiladas en 2005
+
+SELECT 
+    f.title
+FROM
+    film f
+        JOIN
+    film_category fc USING (film_id)
+        JOIN
+    category c USING (category_id)
+WHERE
+    c.name = 'Sports'
+        AND NOT EXISTS( SELECT 
+            1
+        FROM
+            inventory i
+                JOIN
+            rental r USING (inventory_id)
+        WHERE
+            i.film_id = f.film_id
+                AND YEAR(r.rental_date) = 2005);
+                
+-- =EJERCICIOS RESUELTOS=
+
+-- el objetivo es comprender cuando conviene usar una subconsulta derivada, una correclacionada o una escalar.
+
+-- DERIVADA -> Películas por idioma
+
+-- Enunciado: Queremos conocer cuántas películas hay registradas en la base de datos por
+-- cada idioma. Para ello agrupamos las películas según su language_id, contamos cuántas
+-- hay en cada grupo, y posteriormente relacionamos ese resultado con la tabla language para
+-- mostrar el nombre del idioma.
+
+-- Tipo de subconsulta: derivada (tabla intermedia agregada).
+-- Camino: film → (derivada por language_id) → language
+-- Salida: language_id, language_name, films_in_language (desc)
+
+SELECT 
+    l.language_id, l.name, t.films_in_language
+FROM
+    (SELECT 
+        f.language_id, COUNT(*) AS films_in_language
+    FROM
+        film f
+    GROUP BY f.language_id) AS t
+        JOIN
+    language AS l ON l.language_id = t.language_id;
+
+-- Interpretación: La subconsulta crea una tabla temporal (t) que contiene la cantidad de
+-- películas por idioma. Posteriormente se une con language para mostrar el nombre de cada
+-- idioma. El resultado demuestra que todas las películas están en inglés.
+
+-- DERIVADA -> Idiomas con longitud media superior a 110 minutos
+
+SELECT 
+    l.language_id, l.name, s.avg_length
+FROM
+    (SELECT 
+        f.language_id, AVG(f.length) AS avg_length
+    FROM
+        film f
+    GROUP BY f.language_id) AS s
+        JOIN
+    language l USING (language_id)
+WHERE
+    s.avg_length > 110;
+    
+-- 	DERIVADA -> Máximo y mínimo replacement_cost por idioma
+
+SELECT 
+    l.language_id,
+    l.name,
+    r.max_replacement_cost,
+    r.min_replacement_cost
+FROM
+    (SELECT 
+        f.language_id,
+            MAX(f.replacement_cost) AS max_replacement_cost,
+            MIN(replacement_cost) AS min_replacement_cost
+    FROM
+        film f
+    GROUP BY f.language_id) AS r
+        JOIN
+    language l USING (language_id);
+    
+    -- CORRELACIONADA (EXISTS)-> Idiomas con al menos una película rating='R'
+    
+    select * from film limit 10;
+    
+    SELECT 
+    l.language_id, l.name AS language_name
+FROM
+    language l
+WHERE
+    EXISTS( SELECT 
+            1
+        FROM
+            film f
+        WHERE
+            l.language_id = f.language_id
+                AND f.rating = 'R');
+                
+-- ESCALAR -> Número total de idiomas distintos presentes en film
+
+SELECT 
+    (SELECT 
+            COUNT(DISTINCT f.language_id)
+        FROM
+            film f) AS film_languages;
+            
+-- ESCALAR -> Número de películas con calificación 'R'
+
+SELECT 
+    (SELECT 
+            COUNT(f.rating)
+        FROM
+            film f
+        WHERE
+            f.rating = 'R') AS films_rating_r;
+            
+-- CTE (WITH) -> Actores con al menos 30 películas
+    
 
