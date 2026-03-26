8201b2f2ab08ff309d6431ab91d20f47699df80a
Manuel Rey Laguarde <manuelreylaguarde@gmail.com>
2026-03-26 11:47:18 +0100

    continuamos con apuntes y ejercicios de modificación de BBDD

--- Modificacion_BBDD/modificacion_BBDD_apuntes_y_ejemplos_en_clase.sql
+++ Modificacion_BBDD/modificacion_BBDD_apuntes_y_ejemplos_en_clase.sql
@@ -107,8 +107,189 @@ alter table productos
 
 set sql_safe_updates=1;
 
+-- vamos con el 9: modificacion de tipo y conmbre de columna
 
-	
+-- la transaccion es a nivel de datos y no a nivel de estructura
+-- DML= modificar transacciones (datos) -> insert, delete
+-- DDL= definicion -> create, alter, drop, truncate (no afeccta a transaccion)
+start transaction;
+
+alter table productos
+
+change precio_sucio precio decimal (10,2);
+rollback;
+explain productos;
+set sql_safe_updates=0;
+-- estandarización de fechas
+-- 1 comprobamos
+select * from pedidos;
+start transaction;
+-- 2 modificamos a yyyy-mm-dd (formato estandar)
+update pedidos
+-- das un formato de texto y lo cambian, hay que decirle que fechas quiere que leamos
+set fecha_texto = str_to_date(fecha_texto, '%d/%m/%Y')
+where fecha_texto like '%/%/____';
+update pedidos
+set fecha_texto = str_to_date(fecha_texto, '%d-%m-%Y')
+where fecha_texto like '%-%-____';
+update pedidos
+set fecha_texto = str_to_date(fecha_texto, '%Y.%m.%d')
+where fecha_texto like '____.%.%';
+rollback;
+
+start transaction;
+update pedidos
+set fecha_texto = case
+	-- when condicion then valor_a_asignar
+    when fecha_texto like '____.%.%' then str_to_date(fecha_texto, '%Y.%m.%d')
+    when fecha_texto like '%-%-____' then str_to_date(fecha_texto, '%d-%m-%Y')
+    when fecha_texto like '%/%/____' then str_to_date(fecha_texto, '%d/%m/%Y')
+    else fecha_texto end
+    where -- para optimizar QUE HAGA MATCH SOLO CON LAS QUE ESTEN MAL, (agiliza)
+    fecha_texto like '____.%.%' or
+    fecha_texto like '%-%-____' or
+    fecha_texto like '%/%/____';
+-- alter table pedidos change column fecha_texto fecha DATE; -- si hago un alter table hago un commit
+
+-- compruebo que esten bien
+select * from pedidos;
+
+-- y ahora si hago el alter table (que a su vez es commit)
+alter table pedidos change column fecha_texto fecha DATE;
+set sql_safe_updates=1;
+explain pedidos;
+
+-- BLOQUE 3
+
+-- Productos huérfanos: Asigna productos con categoria_id inexistente a la categoria 'General'
+
+-- 1) Que hay mal? productos con categorías que no existen
+select * from productos;
+select * from categorias;
+select *
+from productos p
+left join 
+categorias c on  p.categoria_id = c.id
+where c.id is null;
+
+select *
+from productos
+where categoria_id not in (select id from categorias);
+
+-- corrijo
+set sql_safe_updates=0;
+start transaction;
+update productos
+set categoria_id = (select id from categorias where nombre = 'General') -- anidar consultas
+where categoria_id not in (select id from categorias);
+
+set sql_safe_updates=1;
+commit;
+-- compruebo (la query debe dar 0)
+select count(*)
+from productos
+where categoria_id not in (select id from categorias);
+
+-- 2. Clientes huérfanos: Reasigna pedidos con cliente_id inexistente al 'Cliente Ficticio'.
 
 
 
+-- 3. Deduplicación de clientes: Elimina duplicados manteniendo el ID más bajo. CASO TIPICO
+-- IMPORTANTE: Reasigna primero los pedidos de los clientes que vas a borrar para no perder el historico
+
+-- 1) Analizamos el problema
+select * from clientes;
+start transaction;
+
+delete from clientes 
+where id=4 or id=5; -- esto no sirve porque hay que hacer una query para verlo
+-- ademas me quedan en la tabla pedidos, pedidos hechos por un cliente que no existen
+
+SELECT p.id as id_pedido,p.cliente_id, c1.id,c1.email, c2.id, c2.email
+FROM 
+	pedidos p 
+		JOIN 
+	clientes c1 ON p.cliente_id = c1.id 
+		JOIN 
+	clientes c2 ON c1.email = c2.email
+WHERE c2.id < c1.id;
+set sql_safe_updates=0;
+
+-- pasamos los pedidos del mismo cliente y los unimos a un único cliente
+update pedidos p 
+		JOIN 
+	clientes c1 ON p.cliente_id = c1.id 
+		JOIN 
+	clientes c2 ON c1.email = c2.email
+set p.cliente_id = c2.id
+where c2.id < c1.id;
+
+-- eliminamos clientes duplicados
+delete c1
+from clientes c1
+		JOIN 
+	clientes c2 ON c1.email = c2.email
+    where c2.id < c1.id;
+
+set sql_safe_updates=1;
+commit;
+select * from pedidos;
+select * from clientes;
+
+
+alter table pedidos
+add constraint fk_pedidos_clientes foreign key (cliente_id) references clientes(id)
+on delete restrict on update cascade;
+-- DA ERROR
+-- no funciona porque hay pedidos que han hecho clientes inexistentes. resuelve el 2.3
+select * from categorias;
+
+alter table productos
+add constraint fk_productos_categorias foreign key (categoria_id) references cateogorias(categoria_id)
+on delete restrict on update cascade;
+
+-- drop constraint fk_productos_categorias; -- si la lío elimino el constraint
+
+-- gestion de nulls
+-- coalesce (primera_opcion, segunda_opcion_si_la_primera_es_nula, tercera_opcion_si_la_segunda_es_nula)
+select * from productos;
+
+alter table productos
+add column precio_final decimal(10,2)
+after precio_oferta;
+
+start transaction;
+set sql_safe_updates=0;
+update productos
+set precio_final = coalesce(precio_oferta, precio,999999999);
+-- otra forma
+-- set precio_final = case when precio_oferta is null then precio_oferta when precio_oferta is null and precio is not null then precio
+set sql_safe_updates=1;
+commit;
+
+-- BLOQUE 6
+-- clientes sin pedidos
+select * from clientes where id not in (select cliente_id from pedidos);
+
+CREATE TABLE `historico_clientes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre_completo` varchar(150) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `telefono` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `direccion` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+show tables;
+
+insert into historico_clientes
+select * from clientes where id not in (select cliente_id from pedidos);
+
+select * from historico_clientes;
+
+
+-- existen los savepoint
+-- primeramente start transaction
+-- savepoint nombre_savepoint
+-- rollback to nombre_savepoint
