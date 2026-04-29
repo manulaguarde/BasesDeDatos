bba20d40323b6793ae4737d565a7e3e4c050b7a3
Manuel Rey Laguarde <manuelreylaguarde@gmail.com>
2026-04-29 13:23:18 +0200

    agrego base de datos de prueba para ejercicio grupo alumnos de Java

--- Creacion_base_de_datos/alumno_grupo_bd.sql
+++ Creacion_base_de_datos/alumno_grupo_bd.sql
@@ -1,3 +1,4 @@
+drop database if exists grupo_alumno;
 create database grupo_alumno;
 use grupo_alumno;
 
@@ -5,7 +6,11 @@ create table alumnos(
 	id smallint primary key auto_increment,
     nombre varchar(25) not null,
     edad smallint not null,
-    calificacion smallint,
+    calificacion decimal (10,2),
     constraint chk_calificacion check (calificacion >=0 and calificacion <=10),
     constraint chk_edad check (edad >=17 and edad <=100)
 );
+
+
+
+select * from alumnos;
